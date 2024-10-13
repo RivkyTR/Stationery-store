5c505ed6d8a3e735e076a26d6c65d37296859dc7
RivkyTR <Rivka4555@gmail.com>
2024-10-13 11:09:45 +0300

    merging the html pages

--- data/data.js
+++ data/data.js
@@ -65,6 +65,7 @@ window.onload = (event) => {
         notebook();
     }
  });
+    document.getElementById('shoppingCart').addEventListener("click", shoppingCart);
   };
 
 
@@ -94,4 +95,55 @@ function deleteFromCard(title, price){
 function updateNumberOfChosenProducts(num){
     numberProductes += num;
     document.getElementById("numberOfChosenProducts").innerText = numberProductes;
+}
+
+function shoppingCart(){
+    console.log("shoppingCart");
+    
+    document.getElementById("center-page1").style.display = "none";
+    document.getElementById("center-page2").style.display = "flex";
+    
+}
+
+
+Object.entries(localStorage).forEach(([key, value]) => {
+    let num = JSON.parse(localStorage.getItem(key)).num;
+    let price = JSON.parse(localStorage.getItem(key)).price;
+    const div = document.getElementById("orderd_items");
+    let html = `<div class="orderd_items" id="${key}">
+                        <div class="changeAmaunt">
+                        <button id="${key}-add">+</button>
+                          <span id="${key}-num">${num}</span>  
+                        <button id="${key}-remove">-</button>
+                        </div>
+                        <div> <span>${price}</span></div>
+                         <div> <span>${key}</span> </div>
+
+                    </div>`
+    div.insertAdjacentHTML("beforeend", html);
+    console.log(key, value);
+    // Then attach the event listener
+    document.getElementById(`${key}-add`).addEventListener("click", function () {
+        addToCart(key, price);
+        updateNum(key);
+    });
+    document.getElementById(`${key}-remove`).addEventListener("click", function () {
+        deleteFromCard(key, price);
+        updateNum(key);
+    });
+});
+function updateNum(key) {
+    const div = document.getElementById(`${key}-num`);
+    const item = JSON.parse(localStorage.getItem(key));
+    if (item === null || item === undefined||!item.num) {
+        const div = document.getElementById(`${key}`);
+        if (div) {
+            console.log(key);
+             
+            div.innerHTML="";
+            div.remove();  // מוחק את האלמנט מה-DOM אם num אינו מוגדר
+        }
+        return;
+    }
+    div.innerHTML = `${item.num}`;
 }
